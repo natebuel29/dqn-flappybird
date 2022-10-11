d4a84bc9630519a05cf494e2974fd54d0b17ce38
natebuel29 <natebuel29@gmail.com>
2022-10-10 20:57:16 -0500

    implement bird class

--- game/flappybird.py
+++ game/flappybird.py
@@ -1,22 +1,32 @@
-import pygame as pg
+import pygame
+from sprites import Bird
 import os
 
 FPS = 30
 SCREEN_HEIGHT = 512
 SCREEN_WIDTH = 320
 
-pg.init()
-screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pg.display.set_caption("Flappy Bird")
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Flappy Bird")
 
-background = pg.Surface(screen.get_size())
+background = pygame.Surface(screen.get_size())
 background = background.convert()
-background.fill((170, 238, 187))
-clock = pg.time.Clock()
-
+background.fill((0, 0, 187))
+clock = pygame.time.Clock()
+bird = Bird()
 
 going = True
 while going:
     clock.tick(FPS)
+    should_jump = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                should_jump = True
+    background.fill((0, 0, 187))
+    bird.draw(background, should_jump=should_jump)
     screen.blit(background, (0, 0))
     pygame.display.flip()
