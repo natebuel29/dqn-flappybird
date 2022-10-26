5700017f716bb77ddb11fb52aa66a59da4368b44
natebuel29 <natebuel29@gmail.com>
2022-10-25 21:11:56 -0500

    create dqn class

---
+++ network/dqn.py
@@ -0,0 +1,19 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+
+
+class DQN:
+    def __init__(self, input_shape, number_of_actions):
+        self.input_shape = input_shape
+        self.model = Sequential()
+        self.model.add(Conv2D(filters=32, kernel_size=8,
+                       strides=4, activation="relu", input_shape=input_shape))
+        self.model.add(Conv2D(filters=64, kernel_size=4,
+                       strides=2, activation="relu"))
+        self.model.add(Conv2D(filters=64, kernel_size=3,
+                       strides=1, activation="relu"))
+        self.model.add(Flatten())
+        self.model.add(Dense(1, activation="sigmoid"))
+
+    def forward(self, x):
+        self.model.predict(x)
